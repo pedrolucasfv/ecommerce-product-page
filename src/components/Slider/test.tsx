b82562908435a92829cabd22d9b5a6ebf72391a1
tuktuks <pedro.lucas.tukinha25@gmail.com>
2022-11-17 22:43:04 -0300

    .

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -5,7 +5,7 @@ import Slider from '.'
 
 describe('<Slider />', () => {
   it('should render children as slider item', () => {
-    const { container } = render(
+    render(
       <Slider settings={{ slidesToShow: 1, infinite: false }}>
         <p>Item 1</p>
         <p>Item 2</p>
@@ -18,6 +18,5 @@ describe('<Slider />', () => {
     expect(
       screen.getByText(/item 2/i).parentElement?.parentElement
     ).toHaveClass('slick-slide')
-    expect(container.firstChild).toMatchSnapshot()
   })
 })
